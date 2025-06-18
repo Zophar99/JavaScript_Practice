478bb14c8800287ca21016f95d9e0980147e67b7
zophar <zophar.nyakundi@student.moringaschool.com>
2025-06-19 01:24:04 +0300

    JavaScript-Exercise

---
+++ practice5.js
@@ -0,0 +1,11 @@
+function gradeStudent(score){
+    if (score>=80){
+    console.log('Excellent');}
+    else if(score>=50 && score <=79){
+        console.log( 'Good');
+    }
+    else{
+        console.log('Fail')
+    }
+}
+gradeStudent (58);
