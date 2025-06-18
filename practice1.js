9dce6dae1a806a2e9a85aa02877f28955482b7bf
zophar <zophar.nyakundi@student.moringaschool.com>
2025-06-19 00:30:25 +0300

    JavaScript-Exercise

--- practice1.js
+++ practice1.js
@@ -1,4 +1,4 @@
 function myFavouriteQuote(){
-    console.log("I love God");
+    console.log("Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill");
 }
 myFavouriteQuote()
